b9459036522ea94cae30e5ba39541f1dcbda1602
Charitha Rajapakse <charitha.2016120@iit.ac.lk>
2017-10-29 22:09:13 +0530

    Create HelloWorldCharitha.java

---
+++ scripts/HelloWorldCharitha.java
@@ -0,0 +1,12 @@
+/*
+ * LANGUAGE: JAVA
+ * AUTHOR: Charitha Rajapakse
+ * GITHUB: https://github.com/Charitha96
+ * 
+ * **/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
